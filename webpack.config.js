dcbc407297fc32d029e13e289ca288b8b8b6de61
pablohernanaraujo <pablohernanaraujo@gmail.com>
2017-03-02 03:09:56 -0300

    mejorando el build

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
     'libsodium-native': 'libsodium_native'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {warnings: false},
       output: {comments: false},
